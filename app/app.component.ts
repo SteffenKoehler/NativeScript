27f488b74dae1dfb875ab1507a903531b60053f5
Steffen Köhler <steffen.koehler@sovanta.com>
2017-07-20 16:01:23 +0200

    hide action bar

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,16 @@
 import { Component } from "@angular/core";
+import { Page } from 'tns-core-modules/ui/page';
 
 @Component({
     selector: "ns-app",
     templateUrl: "app.component.html",
 })
-export class AppComponent { }
+export class AppComponent {
+
+    constructor (
+        private page: Page
+    ) {
+        page.actionBarHidden = true;
+    }
+
+}

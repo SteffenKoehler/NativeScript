f1dcf51964ec65355de6b142265e828c5e100765
Steffen Köhler <steffen.koehler@sovanta.com>
2017-07-21 13:13:56 +0200

    created settings component

---
+++ app/pages/tabView/settings/settings.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core'
+import { RouterExtensions } from 'nativescript-angular/router'
+
+
+@Component({
+    moduleId: module.id,
+    selector: 'ns-settings',
+    templateUrl: './settings.component.html',
+    styleUrls: ['./settings.component.css']
+})
+export class SettingsComponent implements OnInit {
+
+    @ViewChild('scrollView') scrollView: ElementRef
+
+    private isLoading = true
+
+    constructor(
+        private router: RouterExtensions,
+    ) { }
+
+    ngOnInit() { }
+
+
+}

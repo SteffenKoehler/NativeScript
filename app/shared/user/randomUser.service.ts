431c16180da769e05d968eee88ec739e0af1c130
Steffen Köhler <steffen.koehler@sovanta.com>
2017-07-21 13:59:34 +0200

    added functionality to store random users in the service and return the list if there are no changes on the number

--- app/shared/user/randomUser.service.ts
+++ app/shared/user/randomUser.service.ts
@@ -3,57 +3,85 @@
  */
 import { Injectable } from "@angular/core";
 import { Http, Headers, Response } from "@angular/http";
-import { Observable } from "rxjs/Rx";
+import { ObjectUnsubscribedError, Observable } from "rxjs/Rx";
 import "rxjs/add/operator/map";
 import {Randomuser} from "./randomUser";
+import { ObservableArray } from 'tns-core-modules/data/observable-array';
 
 
 @Injectable()
 export class RandomuserService {
-    constructor(private http: Http) {}
+    constructor(private http: Http) {
+
+        console.log('############ init random user service')
+    }
 
     numberOfResults: number = 50;
+    makeNewRequest: boolean = true;
+    randomUserArry: Array<Randomuser> = [];
 
 
 
     getUsers(nationalitie) {
-        let headers = new Headers();
-        let url = 'https://randomuser.me/api/';
+        if (this.makeNewRequest) {
+            let headers = new Headers();
+            let url = 'https://randomuser.me/api/';
 
-        url = url + "?results=" + this.numberOfResults + "&nat=" + nationalitie;
+            url = url + "?results=" + this.numberOfResults + "&nat=" + nationalitie;
 
 
-        return this.http.get(url, {
-        })
-            .map(res => res.json())
-            .map(data => {
-                let randomUserList = [];
-                data.results.forEach((randomUser) => {
-                    let name = {
-                        first : randomUser.name.first,
-                        last: randomUser.name.last
-                    };
+            return this.http.get(url, { })
+                .map(res => res.json())
+                .map(data => {
+                    let randomUserList = [];
+                    this.randomUserArry = [];
+                    data.results.forEach((randomUser) => {
+                        let name = {
+                            first : randomUser.name.first,
+                            last: randomUser.name.last
+                        };
 
-                    let picture = {
-                        large: randomUser.picture.large,
-                        medium: randomUser.picture.medium,
-                        thumbnail: randomUser.picture.thumbnail
-                    };
+                        let picture = {
+                            large: randomUser.picture.large,
+                            medium: randomUser.picture.medium,
+                            thumbnail: randomUser.picture.thumbnail
+                        };
 
-                    let location = {
-                        street: randomUser.location.street,
-                        city: randomUser.location.city,
-                        state: randomUser.location.state,
-                        postcode: randomUser.location.postcode
-                    };
+                        let location = {
+                            street: randomUser.location.street,
+                            city: randomUser.location.city,
+                            state: randomUser.location.state,
+                            postcode: randomUser.location.postcode
+                        };
 
-                    let favorite = location.city.length > 8;
+                        let favorite = location.city.length > 8;
 
-                    randomUserList.push(new Randomuser(randomUser.gender, name, picture, location, randomUser.email, randomUser.cell, favorite));
-                });
-                return randomUserList;
-            })
-            .catch(this.handleErrors);
+                        const user = new Randomuser(
+                            randomUser.gender,
+                            name,
+                            picture,
+                            location,
+                            randomUser.email,
+                            randomUser.cell,
+                            favorite
+                        );
+
+                        randomUserList.push(user);
+                        this.randomUserArry.push(user);
+                    });
+                    this.makeNewRequest = false;
+                    return randomUserList;
+                })
+                .catch(this.handleErrors);
+        } else {
+            return Observable.of(this.randomUserArry);
+        }
+    }
+
+
+    setNewNumber(newNumber): void {
+        this.numberOfResults = newNumber;
+        this.makeNewRequest = true;
     }
 
     handleErrors(error: Response) {
